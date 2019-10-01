72075ae28c886d84af9571a88fd7454cee51dfe4
John <johnageronimo1@gmail.com>
2019-10-01 16:07:53 +1300

    setting up tree variables

---
+++ src/TreeList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Trees = () => {
+    return(
+        <div>
+            <h1>Tree Name</h1>
+            <p>Tree Description</p>
+            <p>Tree Price</p>
+            <img src='' alt=''/>
+        </div>
+    );
+}
+
+export default Trees;
